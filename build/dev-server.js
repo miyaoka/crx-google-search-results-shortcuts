095059ef1fff6aaab5decf03b6867a88f15aaa6f
Masaya Kazama <miyan@t-p.jp>
2017-08-07 15:59:41 +0900

    Compile dev server after remove dist dir

--- build/dev-server.js
+++ build/dev-server.js
@@ -27,72 +27,72 @@ var app = express()
 
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
   if (err) throw err
-})
 
-var compiler = webpack(webpackConfig)
+  var compiler = webpack(webpackConfig)
 
-var devMiddleware = require('webpack-dev-middleware')(compiler, {
-  publicPath: webpackConfig.output.publicPath,
-  quiet: true
-})
-
-var hotMiddleware = require('webpack-hot-middleware')(compiler, {
-  log: () => {},
-  heartbeat: 2000
-})
-// force page reload when html-webpack-plugin template changes
-compiler.plugin('compilation', function (compilation) {
-  compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
-    hotMiddleware.publish({ action: 'reload' })
-    cb()
+  var devMiddleware = require('webpack-dev-middleware')(compiler, {
+    publicPath: webpackConfig.output.publicPath,
+    quiet: true
   })
-})
 
-// proxy api requests
-Object.keys(proxyTable).forEach(function (context) {
-  var options = proxyTable[context]
-  if (typeof options === 'string') {
-    options = { target: options }
+  var hotMiddleware = require('webpack-hot-middleware')(compiler, {
+    log: () => {},
+    heartbeat: 2000
+  })
+  // force page reload when html-webpack-plugin template changes
+  compiler.plugin('compilation', function (compilation) {
+    compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
+      hotMiddleware.publish({ action: 'reload' })
+      cb()
+    })
+  })
+
+  // proxy api requests
+  Object.keys(proxyTable).forEach(function (context) {
+    var options = proxyTable[context]
+    if (typeof options === 'string') {
+      options = { target: options }
+    }
+    app.use(proxyMiddleware(options.filter || context, options))
+  })
+
+  // handle fallback for HTML5 history API
+  app.use(require('connect-history-api-fallback')())
+
+  // serve webpack bundle output
+  app.use(devMiddleware)
+
+  // enable hot-reload and state-preserving
+  // compilation error display
+  app.use(hotMiddleware)
+
+  // serve pure static assets
+  var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
+  app.use(staticPath, express.static('./static'))
+
+  var uri = 'http://localhost:' + port
+
+  var _resolve
+  var readyPromise = new Promise(resolve => {
+    _resolve = resolve
+  })
+
+  console.log('> Starting dev server...')
+  devMiddleware.waitUntilValid(() => {
+    console.log('> Listening at ' + uri + '\n')
+    // when env is testing, don't need open it
+    if (autoOpenBrowser && process.env.NODE_ENV !== 'testing') {
+      opn(uri)
+    }
+    _resolve()
+  })
+
+  var server = app.listen(port)
+
+  module.exports = {
+    ready: readyPromise,
+    close: () => {
+      server.close()
+    }
   }
-  app.use(proxyMiddleware(options.filter || context, options))
 })
-
-// handle fallback for HTML5 history API
-app.use(require('connect-history-api-fallback')())
-
-// serve webpack bundle output
-app.use(devMiddleware)
-
-// enable hot-reload and state-preserving
-// compilation error display
-app.use(hotMiddleware)
-
-// serve pure static assets
-var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
-app.use(staticPath, express.static('./static'))
-
-var uri = 'http://localhost:' + port
-
-var _resolve
-var readyPromise = new Promise(resolve => {
-  _resolve = resolve
-})
-
-console.log('> Starting dev server...')
-devMiddleware.waitUntilValid(() => {
-  console.log('> Listening at ' + uri + '\n')
-  // when env is testing, don't need open it
-  if (autoOpenBrowser && process.env.NODE_ENV !== 'testing') {
-    opn(uri)
-  }
-  _resolve()
-})
-
-var server = app.listen(port)
-
-module.exports = {
-  ready: readyPromise,
-  close: () => {
-    server.close()
-  }
-}
