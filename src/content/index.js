617c100a63f58c0f7db281d0bd000c18d21e1a2f
Masaya Kazama <miyan@t-p.jp>
2017-08-07 16:29:51 +0900

    Assign horizontal keys to navigate next/prev page

--- src/content/index.js
+++ src/content/index.js
@@ -2,18 +2,24 @@ import './index.css'
 
 // Elements
 const links = Array.from(document.querySelectorAll('div > h3 > a:first-child, td > a.pn'))
+const nextPageLink = document.querySelector('#pnnext')
+const prevPageLink = document.querySelector('#pnprev')
 const searchInput = document.querySelector('#lst-ib')
 
 // Constant var
 const LAST_INDEX = links.length - 1
 const DOWN_KEYS = ['ArrowDown', 'KeyJ']
 const UP_KEYS = ['ArrowUp', 'KeyK']
+const RIGHT_KEYS = ['ArrowRight', 'KeyL']
+const LEFT_KEYS = ['ArrowLeft', 'KeyH']
 const SEARCH_KEY = 'Slash'
 
 // Methods
 const focus = (i) => links[i].focus()
 const focusDown = () => focus(focusIndex = Math.min(focusIndex + 1, LAST_INDEX))
 const focusUp = () => focus(focusIndex = Math.max(focusIndex - 1, 0))
+const focusRight = () => nextPageLink ? nextPageLink.click() : null
+const focusLeft = () => prevPageLink ? prevPageLink.click() : null
 const focusInput = () => {
   searchInput.focus()
   searchInput.select()
@@ -36,6 +42,10 @@ const init = () => {
           focusDown()
         } else if (UP_KEYS.includes(e.code)) {
           focusUp()
+        } else if (RIGHT_KEYS.includes(e.code)) {
+          focusRight()
+        } else if (LEFT_KEYS.includes(e.code)) {
+          focusLeft()
         } else {
           return
         }
