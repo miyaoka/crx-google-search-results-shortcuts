9b16a36ed518b9016b94c5e01882ba37acbff4d8
Masaya Kazama <miyan@t-p.jp>
2017-11-14 15:50:03 +0900

    Add transition

--- src/options/components/KeyEditor.vue
+++ src/options/components/KeyEditor.vue
@@ -5,35 +5,39 @@
       :key="actionItemIndex"
     >
       <v-subheader>{{actionItem.action}}</v-subheader>
-      <v-list-tile
-        avatar
-        v-for="(hotkey, keyIndex) in actionItem.hotkeys"
-        :key="hotkey.id"
-      >
-        <v-list-tile-avatar>
-        </v-list-tile-avatar>
-        <v-list-tile-content>
-          <key-input
-            :value="hotkey.key"
-            @input="(value, changed) => updateInput(actionItemIndex, keyIndex, value, changed)"
-          ></key-input>
-        </v-list-tile-content>
-        <v-list-tile-action>
-          <v-btn
-            flat icon color="red"
-            @click.native="removeInput(actionItemIndex, keyIndex)"
-          >
-            <v-icon>remove_circle</v-icon>
-          </v-btn>
-        </v-list-tile-action>
-      </v-list-tile>
+      <transition-group tag="div" name="list">
+        <v-list-tile
+          avatar
+          v-for="(hotkey, keyIndex) in actionItem.hotkeys"
+          :key="hotkey.id"
+        >
+          <v-list-tile-avatar>
+          </v-list-tile-avatar>
+          <v-list-tile-content>
+            <key-input
+              :value="hotkey.key"
+              @input="(value, changed) => updateInput(actionItemIndex, keyIndex, value, changed)"
+            ></key-input>
+          </v-list-tile-content>
+          <v-list-tile-action>
+            <v-btn
+              flat
+              icon
+              color="red"
+              @click.native="removeInput(actionItemIndex, keyIndex)"
+            >
+              <v-icon>remove_circle</v-icon>
+            </v-btn>
+          </v-list-tile-action>
+        </v-list-tile>
+      </transition-group>
       <v-list-tile>
         <v-list-tile-avatar>
         </v-list-tile-avatar>
         <v-list-tile-content
         >
           <v-btn
-            color="green"
+            color="teal"
             flat
             outline
             @click.native="addInput(actionItemIndex)"
@@ -46,6 +50,42 @@
       </v-list-tile>
       <v-divider v-if="actionItemIndex + 1 < keyEditList.length"></v-divider>
     </v-list>
+    <v-footer app fixed dark color="teal">
+      <v-container fluid>
+        <v-layout row justify-center>
+          <v-flex class="text-xs-center">
+            <v-btn
+              color="blue-grey"
+              dark
+            >
+              <v-icon>cached</v-icon>
+              Load Defaults
+            </v-btn>
+          </v-flex>
+          <v-flex class="text-xs-center">
+            <v-btn
+              color="blue-grey"
+              dark
+              disabled
+            >
+              <v-icon>settings_backup_restore</v-icon>
+              Reset
+            </v-btn>
+          </v-flex>
+          <v-flex class="text-xs-center">
+            <v-btn
+              color="blue-grey"
+              dark
+              disabled
+            >
+              <v-icon>check_circle</v-icon>
+              Submit
+            </v-btn>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-footer>
+
   </v-container>
 </template>
 
@@ -116,4 +156,30 @@ export default Vue.extend({
 </script>
 
 <style>
+.list-enter-active,
+.list-leave-active,
+.list-move {
+  transition: 300ms cubic-bezier(0.59, 0.12, 0.34, 0.95);
+  transition-property: opacity, transform;
+}
+
+.list-enter {
+  opacity: 0;
+  transform: translateY(-50px) scaleY(0.5);
+}
+
+.list-enter-to {
+  opacity: 1;
+  transform: translateY(0) scaleY(1);
+}
+
+.list-leave-active {
+  position: absolute;
+}
+
+.list-leave-to {
+  opacity: 0;
+  transform: scaleX(0.2) scaleY(0.2);
+  transform-origin: center center;
+}
 </style>
