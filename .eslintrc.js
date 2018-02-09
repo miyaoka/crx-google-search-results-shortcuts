c166eeef045caae2b54f88e09b3ed103efe71a2b
Masaya Kazama <miyan@t-p.jp>
2018-02-09 16:44:17 +0900

    Add prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,19 +7,19 @@ module.exports = {
     sourceType: 'module'
   },
   env: {
-    browser: true,
+    browser: true
   },
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
   extends: 'standard',
   // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
+  plugins: ['html'],
   globals: {
     chrome: false
   },
   // add your custom rules here
-  'rules': {
+  rules: {
+    // for prettier
+    'space-before-function-paren': ['error', 'never'],
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
