2cd2f6f767bee117372ebba34f5d2458fa7c5bf5
Masaya Kazama <miyan@t-p.jp>
2020-01-14 01:53:38 +0900

    add verbatim search key

--- src/scripts/content/index.ts
+++ src/scripts/content/index.ts
@@ -5,7 +5,7 @@ import { MetaSearch } from './MetaSearch'
 const searchResult = new SearchResults()
 const metaSearch = new MetaSearch()
 
-type KeyDefs = [string[], Function][]
+type KeyDefs = [(string | string[])[], Function][]
 
 const keyDefs: KeyDefs = [
   [['ArrowDown', 'KeyJ'], () => searchResult.focusNext()],
@@ -21,25 +21,32 @@ const nextKeyDefs: KeyDefs = [
   [['KeyI'], () => metaSearch.searchImage()],
   [['KeyM'], () => metaSearch.searchMap()],
   [['KeyV'], () => metaSearch.searchVideo()],
-  [['KeyN'], () => metaSearch.searchNews()]
+  [['KeyN'], () => metaSearch.searchNews()],
+  [[['shift', 'KeyV']], () => metaSearch.searchVerbatim()]
 
   // [['KeyH'], () => metaSearch.searchByTime('qdr_h')],
   // [['KeyD'], () => metaSearch.searchByTime('qdr_d')],
   // [['KeyW'], () => metaSearch.searchByTime('qdr_w')],
   // [['KeyM'], () => metaSearch.searchByTime('qdr_m')],
   // [['KeyY'], () => metaSearch.searchByTime('qdr_y')],
-  // [['KeyV'], () => metaSearch.searchVerbatim()]
 ]
 
+const combineKey = (keys: string[]) => keys.sort().join('-')
 const createKeyMap = (keyDefs: KeyDefs): [RegExp, Function][] => {
-  return keyDefs.map(([keys, action]) => {
-    return [new RegExp(`^(${keys.join('|')})$`), action]
+  return keyDefs.map(([keyCombos, action]) => {
+    const pattern = keyCombos
+      .map(keyCombo =>
+        Array.isArray(keyCombo) ? combineKey(keyCombo) : keyCombo
+      )
+      .join('|')
+    return [new RegExp(`^(${pattern})$`), action]
   })
 }
 
 const keymap = createKeyMap(keyDefs)
 const nextKeymap = createKeyMap(nextKeyDefs)
 
+console.log(nextKeymap)
 const getCombinedKeyCode = (e: KeyboardEvent) => {
   const modKeyMap = {
     shift: e.shiftKey,
@@ -47,15 +54,13 @@ const getCombinedKeyCode = (e: KeyboardEvent) => {
     ctrl: e.ctrlKey,
     meta: e.metaKey
   }
-  return Object.entries(modKeyMap)
-    .reduce(
-      (acc: string[], [key, isHolding]) => {
-        return isHolding ? [...acc, key] : acc
-      },
-      [e.code]
-    )
-    .sort()
-    .join('+')
+  const keys = Object.entries(modKeyMap).reduce(
+    (acc: string[], [key, isHolding]) => {
+      return isHolding ? [...acc, key] : acc
+    },
+    [e.code]
+  )
+  return combineKey(keys)
 }
 
 const onKeyDown = (e: KeyboardEvent) => {
@@ -85,10 +90,10 @@ const onKeyDown = (e: KeyboardEvent) => {
 
 const activateKeyHandler = (isActive: boolean) => {
   if (isActive) {
-    document.addEventListener('keydown', onKeyDown)
+    document.addEventListener('keyup', onKeyDown)
     searchResult.resetFocus()
   } else {
-    document.removeEventListener('keydown', onKeyDown)
+    document.removeEventListener('keyup', onKeyDown)
   }
 }
 
