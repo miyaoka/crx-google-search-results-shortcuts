b844ab865ae5526aa27528b3d5a89fa440f18648
Masaya Kazama <miyan@t-p.jp>
2020-01-13 19:08:32 +0900

    Add meta search keys

--- src/scripts/content/index.ts
+++ src/scripts/content/index.ts
@@ -1,29 +1,78 @@
 import './index.scss'
-import SearchResult from './searchResult'
+import { SearchResults } from './SearchResults'
+import { MetaSearch } from './MetaSearch'
 
-const searchResult = new SearchResult()
+const searchResult = new SearchResults()
+const metaSearch = new MetaSearch()
 
-const formInputs = document.querySelectorAll('input, textarea')
+type KeyDefs = [string[], Function][]
 
-const keyDefs: [string[], Function][] = [
+const keyDefs: KeyDefs = [
   [['ArrowDown', 'KeyJ'], () => searchResult.focusNext()],
   [['ArrowUp', 'KeyK'], () => searchResult.focusPrev()],
   [['ArrowRight', 'KeyL'], () => searchResult.moveToNextPage()],
   [['ArrowLeft', 'KeyH'], () => searchResult.moveToPrevPage()],
-  [['Slash'], () => searchResult.focusInput()]
+  [['Slash'], () => searchResult.focusInput()],
+  [['KeyG'], () => metaSearch.setLeaderKey()],
+
+  [['KeyA'], () => metaSearch.searchAll()],
+  [['KeyI'], () => metaSearch.searchImage()],
+  [['KeyM'], () => metaSearch.searchMap()],
+  [['KeyV'], () => metaSearch.searchVideo()],
+  [['KeyN'], () => metaSearch.searchNews()]
 ]
 
-const keymap: [RegExp, Function][] = keyDefs.map(([keys, action]) => {
-  return [new RegExp(`^${keys.join('|')}$`), action]
-})
+const nextKeyDefs: KeyDefs = [
+  [['KeyH'], () => metaSearch.searchByTime('qdr_h')],
+  [['KeyD'], () => metaSearch.searchByTime('qdr_d')],
+  [['KeyW'], () => metaSearch.searchByTime('qdr_w')],
+  [['KeyM'], () => metaSearch.searchByTime('qdr_m')],
+  [['KeyY'], () => metaSearch.searchByTime('qdr_y')],
+  [['KeyV'], () => metaSearch.searchVerbatim()]
+]
+
+const createKeyMap = (keyDefs: KeyDefs): [RegExp, Function][] => {
+  return keyDefs.map(([keys, action]) => {
+    return [new RegExp(`^(${keys.join('|')})$`), action]
+  })
+}
+
+const keymap = createKeyMap(keyDefs)
+const nextKeymap = createKeyMap(nextKeyDefs)
+
+const getCombinedKeyCode = (e: KeyboardEvent) => {
+  const modKeyMap = {
+    shift: e.shiftKey,
+    alt: e.altKey,
+    ctrl: e.ctrlKey,
+    meta: e.metaKey
+  }
+  const holdingModKeys = Object.entries(modKeyMap)
+    .reduce((acc: string[], [key, isHolding]) => {
+      return isHolding ? [...acc, key] : acc
+    }, [])
+    .sort()
+  return [...holdingModKeys, e.code].join('+')
+}
 
 const onKeyDown = (e: KeyboardEvent) => {
-  const modKeys = [e.shiftKey, e.altKey, e.ctrlKey, e.metaKey]
-  // Ignore when input has modifier keys
-  if (modKeys.some(Boolean)) return
+  const code = getCombinedKeyCode(e)
+
+  if (metaSearch.isLeaderKey) {
+    metaSearch.isLeaderKey = false
+    const matched = nextKeymap.some(([keyReg, action]) => {
+      if (!keyReg.test(code)) return false
+      if (action()) {
+        e.preventDefault()
+      }
+      return true
+    })
+
+    if (matched) return
+  }
 
   keymap.some(([keyReg, action]) => {
-    if (!keyReg.test(e.code)) return false
+    if (!keyReg.test(code)) return false
     if (action()) {
       e.preventDefault()
     }
@@ -41,7 +90,8 @@ const activateKeyHandler = (isActive: boolean) => {
 }
 
 const init = () => {
-  if (searchResult.isEmpty) return
+  // if (searchResult.isEmpty) return
+  const formInputs = document.querySelectorAll('input, textarea')
 
   formInputs.forEach(el => {
     el.addEventListener('focusin', () => activateKeyHandler(false))
