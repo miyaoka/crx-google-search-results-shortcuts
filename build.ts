b4b043ceac85240afbe74881292f1ea57ac441fe
Masaya Kazama <miyan@t-p.jp>
2022-01-18 08:54:26 +0900

    WIP

--- build.ts
+++ build.ts
@@ -1,5 +1,42 @@
-import { build } from 'esbuild'
+import { build } from 'esbuild';
+import path from 'path';
+import glob from 'glob';
+import chokidar from 'chokidar';
+import fs from 'fs';
 
+const distDir = path.resolve('./dist');
+const staticDir = path.resolve('./src/static');
+if (fs.existsSync(distDir)) {
+  fs.rmSync(distDir, { recursive: true });
+}
+
+const isWatching = true;
+
+// build scripts
 build({
-  entryPoints: ['./src/**']
-})
+  entryPoints: glob.sync('./src/scripts/*'),
+  bundle: true,
+  outdir: distDir,
+  watch: isWatching
+    ? {
+        onRebuild(error, _result) {
+          if (!error) console.log('watch build succeeded');
+        }
+      }
+    : false
+});
+
+// sync static file
+chokidar
+  .watch(staticDir, { persistent: isWatching })
+  .on('all', (evt, filepath) => {
+    const distFilepath = path.join(distDir, path.relative(staticDir, filepath));
+    if (evt === 'add') {
+      fs.mkdirSync(path.dirname(distFilepath), { recursive: true });
+      fs.copyFileSync(filepath, distFilepath);
+    } else if (evt === 'unlink') {
+      fs.unlinkSync(distFilepath);
+    }
+  });
+
+// build manifest
--- src/scripts/content/index.ts
+++ src/scripts/content/index.ts
@@ -1,11 +1,10 @@
-import './index.scss'
-import { SearchResults } from './SearchResults'
-import { MetaSearch } from './MetaSearch'
+import { SearchResults } from './SearchResults';
+import { MetaSearch } from './MetaSearch';
 
-const searchResult = new SearchResults()
-const metaSearch = new MetaSearch()
+const searchResult = new SearchResults();
+const metaSearch = new MetaSearch();
 
-type KeyDefs = [(string | string[])[], Function][]
+type KeyDefs = [(string | string[])[], Function][];
 
 const keyDefs: KeyDefs = [
   [['ArrowDown', 'j'], () => searchResult.focusNext()],
@@ -14,7 +13,7 @@ const keyDefs: KeyDefs = [
   [['ArrowLeft', 'h'], () => searchResult.moveToPrevPage()],
   [['/'], () => searchResult.focusInput()],
   [['g'], () => metaSearch.setLeaderKey()]
-]
+];
 
 const nextKeyDefs: KeyDefs = [
   [['a'], () => metaSearch.searchAll()],
@@ -29,27 +28,27 @@ const nextKeyDefs: KeyDefs = [
   // [['w'], () => metaSearch.searchByTime('qdr_w')],
   // [['m'], () => metaSearch.searchByTime('qdr_m')],
   // [['y'], () => metaSearch.searchByTime('qdr_y')],
-]
+];
 
 const combineKey = (keys: string[]) =>
   keys
     .map(key => key.toLowerCase())
     .sort()
-    .join('-')
+    .join('-');
 const createKeyMap = (keyDefs: KeyDefs): [RegExp, Function][] => {
   return keyDefs.map(([keyCombos, action]) => {
     const pattern = keyCombos
       .map(keyCombo =>
         Array.isArray(keyCombo) ? combineKey(keyCombo) : keyCombo
       )
-      .join('|')
-    return [new RegExp(`^(${pattern})$`, 'i'), action]
-  })
-}
+      .join('|');
+    return [new RegExp(`^(${pattern})$`, 'i'), action];
+  });
+};
 
-const keymap = createKeyMap(keyDefs)
-const nextKeymap = createKeyMap(nextKeyDefs)
-const modKeyReg = /^(shift|alt|control|meta)$/i
+const keymap = createKeyMap(keyDefs);
+const nextKeymap = createKeyMap(nextKeyDefs);
+const modKeyReg = /^(shift|alt|control|meta)$/i;
 
 const getCombinedKeyCode = (e: KeyboardEvent) => {
   const modKeyMap = {
@@ -57,61 +56,61 @@ const getCombinedKeyCode = (e: KeyboardEvent) => {
     alt: e.altKey,
     ctrl: e.ctrlKey,
     meta: e.metaKey
-  }
+  };
   const keys = Object.entries(modKeyMap).reduce(
     (acc: string[], [key, isHolding]) => {
-      return isHolding ? [...acc, key] : acc
+      return isHolding ? [...acc, key] : acc;
     },
     [e.key]
-  )
-  return combineKey(keys)
-}
+  );
+  return combineKey(keys);
+};
 
 const onKeyDown = (e: KeyboardEvent) => {
-  if (modKeyReg.test(e.key)) return
+  if (modKeyReg.test(e.key)) return;
 
-  const code = getCombinedKeyCode(e)
+  const code = getCombinedKeyCode(e);
 
   if (metaSearch.isLeaderKey) {
-    metaSearch.isLeaderKey = false
+    metaSearch.isLeaderKey = false;
     const matched = nextKeymap.some(([keyReg, action]) => {
-      if (!keyReg.test(code)) return false
+      if (!keyReg.test(code)) return false;
       if (action()) {
-        e.preventDefault()
+        e.preventDefault();
       }
-      return true
-    })
+      return true;
+    });
 
-    if (matched) return
+    if (matched) return;
   }
 
   keymap.some(([keyReg, action]) => {
-    if (!keyReg.test(code)) return false
+    if (!keyReg.test(code)) return false;
     if (action()) {
-      e.preventDefault()
+      e.preventDefault();
     }
-    return true
-  })
-}
+    return true;
+  });
+};
 
 const activateKeyHandler = (isActive: boolean) => {
   if (isActive) {
-    document.addEventListener('keydown', onKeyDown)
-    searchResult.resetFocus()
+    document.addEventListener('keydown', onKeyDown);
+    searchResult.resetFocus();
   } else {
-    document.removeEventListener('keydown', onKeyDown)
+    document.removeEventListener('keydown', onKeyDown);
   }
-}
+};
 
 const init = () => {
-  const formInputs = document.querySelectorAll('input, textarea')
+  const formInputs = document.querySelectorAll('input, textarea');
 
   formInputs.forEach(el => {
-    el.addEventListener('focusin', () => activateKeyHandler(false))
-    el.addEventListener('focusout', () => activateKeyHandler(true))
-  })
+    el.addEventListener('focusin', () => activateKeyHandler(false));
+    el.addEventListener('focusout', () => activateKeyHandler(true));
+  });
 
-  activateKeyHandler(true)
-}
+  activateKeyHandler(true);
+};
 
-init()
+init();
